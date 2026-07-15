26fcfaa2549643b44c6055371602cf72c5d74fc8
Sujal Atkar <atkarsujal@gmail.com>
2026-07-15 19:58:19 +0530

    ADD DSA Question : Product of Array Except Self

---
+++ 01-Arrays/ProdcutofArrayExceptSelf.java
@@ -0,0 +1,62 @@
+/*
+Problem:
+Given an integer array nums, return an array resultwer such that resultwer[i]
+is equal to the product of all the elements of nums except nums[i].
+
+Platform:
+LeetCode 238 - Product of Array Except Self
+
+Approach:
+1. Traverse left to right to compute prefix products.
+2. Traverse right to left to compute suffix products.
+3. Multiply prefix and suffix values.
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(1) extra space (excluding output array)
+*/
+
+import java.util.Scanner;
+
+public class ProdcutofArrayExceptSelf {
+    
+        public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int result[] = new int[n];
+        int prefix = 1;
+        int suffix = 1;
+
+        result[0] = 1;
+
+        // Prefix and Suffix Array
+        for(int i = 1; i<n; i++){
+            prefix *= nums[i-1];
+            result[i] = prefix;
+        }
+
+        for(int i = n-2; i>=0; i--){
+            suffix *= nums[i+1];
+            result[i] *= suffix;
+        }
+        
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result[] = productExceptSelf(arr);
+        for (int value : result) {
+            
+            System.out.print(value +" ");
+        }
+        sc.close();
+    }
+}
